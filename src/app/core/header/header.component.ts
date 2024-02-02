c570e87535999f0d93f1e39ca7129afcb39e6b0b
cadufelicioandrade <cadufelicioandrade@gmail.com>
2024-02-02 10:41:29 -0300

    Update-project 2024-02-02v1

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,8 @@
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+import { UserService } from './../user/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector:'app-header',
@@ -6,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit{
 
-  constructor(){}
+  user$:Observable<User>;
+
+  constructor(
+    private userService:UserService,
+    private router:Router
+    ){
+    this.user$ = this.userService.getUser();
+  }
 
   ngOnInit(): void {
 
+  }
+
+  logout(){
+    this.userService.logout();
+    this.router.navigate(['']);
   }
 }
--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { TokenService } from './../token-service/token.service';
 import { Injectable } from '@angular/core';
 import { User } from './user';
@@ -6,26 +6,40 @@ import { User } from './user';
 @Injectable({providedIn:'root'})
 export class UserService{
 
-  private userSubject: Subject<User>;
+  private userSubject = new BehaviorSubject<User>({name:'Log in!',email:'',id:0});
+  private userName: string;
 
   constructor(private tokenService:TokenService){
     this.tokenService.hasToken() && this.decodeAndNotify();
   }
 
-  setToken(token:string){
+  setToken(token:string):void{
     this.tokenService.setToken(token);
     this.decodeAndNotify();
   }
 
-  getUser(){
+  getUser():Observable<User>{
     return this.userSubject.asObservable();
   }
 
-  private decodeAndNotify(){
+  logout():void{
+    this.tokenService.removeToken();
+    this.userSubject.next({name:'Log in!',email:'',id:0});
+  }
+
+  private decodeAndNotify():void{
     const token = this.tokenService.getToken();
     const jwt_decode = require('jwt-decode');
     const user = jwt_decode(token) as User;
+    this.userName = user.name;
     this.userSubject.next(user);
   }
 
+  isLogged():boolean{
+    return this.tokenService.hasToken();
+  }
+
+  getUserName():string{
+    return this.userName;
+  }
 }
